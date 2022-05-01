208ae019d83e60ff36875caeea27e74fe862b6cd
ElenaSharnina <92175599+ElenaSharnina@users.noreply.github.com>
2022-05-01 11:50:37 +0300

    все роуты пользователя работают

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,12 @@
 const userRouter = require('express').Router();
-const { getUsers, getUserById, createUser } = require('../controllers/users');
+const {
+  getUsers, getUserById, createUser, updateAvatar, updateProfileInfo,
+} = require('../controllers/users');
 
 userRouter.get('/', getUsers);
 userRouter.get('/:userId', getUserById);
 userRouter.post('/', createUser);
+userRouter.patch('/me', updateProfileInfo);
+userRouter.patch('/me/avatar', updateAvatar);
 
 module.exports = userRouter;
