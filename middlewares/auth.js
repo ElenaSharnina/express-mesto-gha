0807df644a60cf9030c133aa5870df28707e1a90
ElenaSharnina <92175599+ElenaSharnina@users.noreply.github.com>
2022-05-15 20:24:16 +0300

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,16 +4,16 @@ const UnauthorizedError = require('../errors/unauthorized-error');
 
 module.exports = (req, res, next) => {
   // eslint-disable-next-line max-len
-  const { authorization } = req.headers; // через это не проходят автотесты!!!! через куки не работет постман
+  // const { authorization } = req.headers; // через это не проходят автотесты!!!! через куки не работет постман
 
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    res.status(401).send({ message: 'Необходима авторизация' });
-  }
-  // if (!req.cookies.jwt) {
-  //   throw new UnauthorizedError('Необходима авторизация');
+  // if (!authorization || !authorization.startsWith('Bearer ')) {
+  //   res.status(401).send({ message: 'Необходима авторизация' });
   // }
-  const token = authorization.replace('Bearer ', '');
-  // const token = req.cookies.jwt;
+  if (!req.cookies.jwt) {
+    throw new UnauthorizedError('Необходима авторизация');
+  }
+  // const token = authorization.replace('Bearer ', '');
+  const token = req.cookies.jwt;
   let payload;
 
   try {
