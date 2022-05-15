2b463857719189746627226c3170100ede9feaf6
ElenaSharnina <92175599+ElenaSharnina@users.noreply.github.com>
2022-05-15 22:31:21 +0300

    Update cards.js

--- controllers/cards.js
+++ controllers/cards.js
@@ -55,7 +55,8 @@ module.exports.likeCard = (req, res) => {
   )
     .then((card) => {
       if (!card) {
-        throw new NotFoundError('Лайк с несуществующим в БД id');
+        res.status(404).send({ message: 'Лайк с несуществующим в БД id' });
+        // throw new NotFoundError('Лайк с несуществующим в БД id');
       } else {
         res.send({ data: card });
       }
