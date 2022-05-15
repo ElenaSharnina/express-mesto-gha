ab805175cd9e78306c56106d7a89b3b88c73df3e
ElenaSharnina <92175599+ElenaSharnina@users.noreply.github.com>
2022-05-15 15:42:57 +0300

    убираем куки

--- controllers/users.js
+++ controllers/users.js
@@ -14,12 +14,13 @@ module.exports.login = (req, res, next) => {
       // создадим токен
       const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
       // вернём токен
-      res.cookie('jwt', token, {
-        maxAge: 3600000 * 24 * 7,
-        httpOnly: true,
-        sameSite: 'none',
-        secure: true,
-      }).send({ token });
+      // res.cookie('jwt', token, {
+      //   maxAge: 3600000 * 24 * 7,
+      //   httpOnly: true,
+      //   sameSite: 'none',
+      //   secure: true,
+      // })
+      res.status(200).send(token);
     })
     .catch(() => {
       // ошибка аутентификации
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,17 +3,18 @@ const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/unauthorized-error');
 
 module.exports = (req, res, next) => {
-  // const { authorization } = req.headers; // через это не проходят автотесты!!!!
+  // eslint-disable-next-line max-len
+  const { authorization } = req.headers; // через это не проходят автотесты!!!! через куки не работет постман
 
-  // if (!authorization || !authorization.startsWith('Bearer ')) {
-  //   throw new UnauthorizedError('Необходима авторизация');
-  // }
-  if (!req.cookies.jwt) {
+  if (!authorization || !authorization.startsWith('Bearer ')) {
     throw new UnauthorizedError('Необходима авторизация');
   }
-  // const token = authorization.replace('Bearer ', '');
+  // if (!req.cookies.jwt) {
+  //   throw new UnauthorizedError('Необходима авторизация');
+  // }
+  const token = authorization.replace('Bearer ', '');
 
-  const token = req.cookies.jwt;
+  // const token = req.cookies.jwt;
   let payload;
 
   try {
